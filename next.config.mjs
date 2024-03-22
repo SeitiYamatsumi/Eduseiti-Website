c160ceccd2f1945123b21da63e0c544b1d8badf1
Eduardo Seiti <eduseiti@hotmail.com>
2024-03-21 23:01:10 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Enable client-side rendering for specific pages
-    async generateStaticParams() {
-      return {
-        '/links': { page: '/links', asPath: '/links' },
-        // Add more paths here if needed
-      };
-    },
   
+    // Add other Next.js configurations as needed
     // Optional: Add your webpack configurations if needed
     // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     //   // Add your webpack configurations here.
