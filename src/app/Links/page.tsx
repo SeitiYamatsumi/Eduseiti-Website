ec44c9b21d88aecd775570f01588f03aaaca49c4
Eduardo Seiti <eduseiti@hotmail.com>
2024-10-04 15:15:09 +0900

    Added Github

--- src/app/Links/page.tsx
+++ src/app/Links/page.tsx
@@ -1,10 +1,17 @@
-'use client';
-import { useEffect } from 'react';
-import * as THREE from 'three';
-import { ChakraProvider, Flex, Image, Box, Heading, extendTheme } from '@chakra-ui/react';
-import LinkCard from '../components/LinkCard';
-import '../globals.css';
-import'@fontsource/poppins';
+"use client";
+import { useEffect } from "react";
+import * as THREE from "three";
+import {
+  ChakraProvider,
+  Flex,
+  Image,
+  Box,
+  Heading,
+  extendTheme,
+} from "@chakra-ui/react";
+import LinkCard from "../components/LinkCard";
+import "../globals.css";
+import "@fontsource/poppins";
 
 const theme = extendTheme({
   fonts: {
@@ -21,31 +28,36 @@ interface Link {
 
 const links: Link[] = [
   {
-    title: 'LinkedIn',
-    description: 'Professional profile',
-    url: 'https://www.linkedin.com/in/eduardo-seiti-yamatsumi/',
+    title: "LinkedIn",
+    description: "Professional profile",
+    url: "https://www.linkedin.com/in/eduardo-seiti-yamatsumi/",
   },
   {
-    title: 'Instagram',
-    description: 'Personal profile',
-    url: 'https://www.instagram.com/seiti.y/',
+    title: "Instagram",
+    description: "Personal profile",
+    url: "https://www.instagram.com/seiti.y/",
   },
   {
-    title: 'LINE',
-    description: 'line profile',
-    url: 'https://line.me/ti/p/s87PENk_EU',
-  }
+    title: "Github",
+    description: "Github profile",
+    url: "https://github.com/SeitiYamatsumi",
+  },
 
   // Add more links as needed
 ];
 
 const Links: React.FC = () => {
   useEffect(() => {
-    const canvas = document.getElementById('MyCanvas') as HTMLCanvasElement;
+    const canvas = document.getElementById("MyCanvas") as HTMLCanvasElement;
 
     const scene = new THREE.Scene();
 
-    const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      canvas.clientWidth / canvas.clientHeight,
+      0.1,
+      1000
+    );
 
     const renderer = new THREE.WebGLRenderer({
       canvas: canvas,
@@ -60,41 +72,47 @@ const Links: React.FC = () => {
       renderer.setSize(newWidth, newHeight);
     };
 
-    window.addEventListener('resize', handleResize);
-    
+    window.addEventListener("resize", handleResize);
+
     const geometry = new THREE.SphereGeometry(1, 32, 32);
     const textureLoader = new THREE.TextureLoader();
-    const texture = textureLoader.load('./assets/fullmapb.webp'); // Replace with the path to your image
+    const texture = textureLoader.load("./assets/fullmapb.webp"); // Replace with the path to your image
     const material = new THREE.MeshStandardMaterial({ map: texture });
     const sphere = new THREE.Mesh(geometry, material);
     scene.add(sphere);
 
     // Load Milky Way texture
     const loader = new THREE.TextureLoader();
-    const milkyWayTexture = loader.load('./assets/milky way.webp');
-    
+    const milkyWayTexture = loader.load("./assets/milky way.webp");
+
     // Create sphere for the Milky Way background
     const sphereGeometry = new THREE.SphereGeometry(900, 32, 32); // Large sphere to encompass the scene
-    const sphereMaterial = new THREE.MeshBasicMaterial({ map: milkyWayTexture, side: THREE.BackSide });
+    const sphereMaterial = new THREE.MeshBasicMaterial({
+      map: milkyWayTexture,
+      side: THREE.BackSide,
+    });
     const backgroundSphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
     // Reduce the brightness by adjusting the color
     backgroundSphere.material.color = new THREE.Color(0.13, 0.08, 0.3); // Adjust the RGB values as needed
     scene.add(backgroundSphere);
-    sphereGeometry.rotateZ(Math.PI /7)
-    sphereGeometry.rotateY(Math.PI /4)
-// Function to create random stars
+    sphereGeometry.rotateZ(Math.PI / 7);
+    sphereGeometry.rotateY(Math.PI / 4);
+    // Function to create random stars
     function createStars() {
       const starsGeometry = new THREE.BufferGeometry();
       const starsPositions = [];
 
       for (let i = 0; i < 1000; i++) {
-          const x = THREE.MathUtils.randFloatSpread(800);
-          const y = THREE.MathUtils.randFloatSpread(800);
-          const z = THREE.MathUtils.randFloatSpread(800);
-          starsPositions.push(x, y, z);
+        const x = THREE.MathUtils.randFloatSpread(800);
+        const y = THREE.MathUtils.randFloatSpread(800);
+        const z = THREE.MathUtils.randFloatSpread(800);
+        starsPositions.push(x, y, z);
       }
 
-      starsGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starsPositions, 3));
+      starsGeometry.setAttribute(
+        "position",
+        new THREE.Float32BufferAttribute(starsPositions, 3)
+      );
 
       const starsMaterial = new THREE.PointsMaterial({ color: 0xffefdf });
       const starField = new THREE.Points(starsGeometry, starsMaterial);
@@ -103,13 +121,13 @@ const Links: React.FC = () => {
 
     // Call the function to create stars
     createStars();
-    
+
     camera.position.z = 7;
     camera.position.y = 2;
     camera.position.x = -6;
 
     const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
-    directionalLight.position.set(1, 5 ,10);
+    directionalLight.position.set(1, 5, 10);
     directionalLight.castShadow = true;
     scene.add(directionalLight);
 
@@ -119,28 +137,24 @@ const Links: React.FC = () => {
       const diagonalMovement = 0.01 * Math.sqrt(2); // Adjust the factor for desired speed
 
       // Update position based on rotation
-      camera.position.x += 2* diagonalMovement * Math.sin(sphere.rotation.y);
+      camera.position.x += 2 * diagonalMovement * Math.sin(sphere.rotation.y);
       camera.position.y += 0.3 * diagonalMovement * Math.sin(sphere.rotation.y);
       camera.position.z += diagonalMovement * Math.cos(sphere.rotation.y);
 
       requestAnimationFrame(animateSphere);
       renderer.render(scene, camera);
     };
-    
-    
+
     animateSphere();
 
-    
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
-    <div style={{ position: "relative", maxHeight: "100%", overflow: "auto"}}>
-      
+    <div style={{ position: "relative", maxHeight: "100%", overflow: "auto" }}>
       <ChakraProvider theme={theme}>
-        
         <Flex
           direction="column"
           align="center"
@@ -159,7 +173,7 @@ const Links: React.FC = () => {
             borderRadius="full"
             fit="cover"
           />
-          <Heading size="md" color="white" textAlign="center" pt={2} pb={8} >
+          <Heading size="md" color="white" textAlign="center" pt={2} pb={8}>
             Yamatsumi Eduardo Seiti
           </Heading>
           <Box width="70%" maxWidth="1200px">
@@ -169,14 +183,17 @@ const Links: React.FC = () => {
           </Box>
         </Flex>
       </ChakraProvider>
-      <canvas id="MyCanvas" style={{
-        position: "fixed",
-        top: 0,
-        width: "100%",
-        height: "100vh",
-        zIndex: 0
-      }}></canvas>
-      </div>
+      <canvas
+        id="MyCanvas"
+        style={{
+          position: "fixed",
+          top: 0,
+          width: "100%",
+          height: "100vh",
+          zIndex: 0,
+        }}
+      ></canvas>
+    </div>
   );
 };
 
